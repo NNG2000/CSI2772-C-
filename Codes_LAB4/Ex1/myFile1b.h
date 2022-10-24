6e0107803affea3658336af4ee95ba63afbf3ec4
Jazia2003 <113474089+Jazia2003@users.noreply.github.com>
2022-10-23 23:12:20 -0400

    Update myFile1b.h

--- Codes_LAB4/Ex1/myFile1b.h
+++ Codes_LAB4/Ex1/myFile1b.h
@@ -18,3 +18,46 @@ public:
 	bool addCourse(Course*, int);
 };
 
+Student::Student(int id, int maxC) {
+	numID = id;
+	maxCourses = maxC;
+	nbCourses = 0;
+	List_grades = new int[maxCourses];
+	List_courses = new Course*[maxCourses];
+
+}
+
+Student::~Student() {
+	delete List_grades;
+	delete List_courses;
+}
+
+double Student::average() {
+	double sum = 0;
+	for (int i = 0; i < nbCourses; i++) {
+		sum += *(List_grades + i);
+	}
+	return sum / nbCourses;
+
+}
+
+int Student::totalHours() {
+	int sum = 0;
+	if (nbCourses > 0) {
+		for (int i = 0; i < nbCourses; i++) {
+			sum += (*(List_courses + i))->getHours();
+		}
+		return sum;
+	}
+	return 0;
+}
+
+
+bool Student::addCourse(Course* c, int grade){
+	if (maxCourses == nbCourses)
+		return false;
+	List_grades[nbCourses] = grade;
+	List_courses[nbCourses] = c;
+	nbCourses++;
+	return true;
+}
