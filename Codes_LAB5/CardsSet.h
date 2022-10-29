8404a28289314634ecc3084348c2b14d617f5dbd
Jazia2003 <jdjou036@uottawa.ca>
2022-10-29 09:41:28 -0400

    methodes question 2

--- Codes_LAB5/CardsSet.h
+++ Codes_LAB5/CardsSet.h
@@ -1,3 +1,5 @@
+#include "myFile.h"
+
 class CardsSet {
 public:
     CardsSet() : number(0) {}
@@ -12,3 +14,45 @@ private:
     Card set[52];
     int number;
 };
+
+void CardsSet::novSet() {
+    int s = 0;
+    for (int i = 0; i < 4; i++) {
+        for (int j = 1; j < 14; j++) {
+            set[s] = Card(static_cast<color>(i), j);
+        }
+    }
+}
+
+void CardsSet::shuffle() {
+    srand(time(0));
+    int nc = number;
+    Card *secondset=new Card[nc];
+    int *num=new int[nc];
+    for (int n = 0; n < nc; n++)
+        num[n] = n;
+    random_shuffle(num, num + nc);
+
+    for (int i = 0; i < nc; i++)
+        secondset[i]=set[num[i]];
+
+    for (int i = 0; i < nc; i++)
+        set[i] = secondset[i];
+}
+
+Card CardsSet::take() {
+    return set[--number];
+}
+
+void CardsSet::put(Card k) {
+    if (number < 52)
+        set[number++] = k;
+}
+
+Card CardsSet::lookIn(int no) {
+    if (no > number)
+        return NULL;
+    int position = number - no;
+    return set[position];
+
+}
