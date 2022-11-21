46a55b07ecf2d8d4eecf080ea877481ba94af263
NNG2000 <55443423+NNG2000@users.noreply.github.com>
2022-11-21 01:59:09 -0500

    I create the card class but I've not done yet

---
+++ Projet_Final/Card.h
@@ -0,0 +1,220 @@
+﻿#include <iostream>
+using namespace std;
+#include <cassert> //for assert()
+#include <string.h>
+
+enum color { club, diamond, heart, spade };
+
+class Card
+{   
+    public virtual string getName();
+    public virtual int getCardsPerCoin(int coins);
+    public virtual void print(ostream& out);
+    private string name;
+    int point;
+
+
+};
+
+class Blue : public virtual card {
+    const maxCard = 20;
+    name = "Blue";
+    private string[20] = (name);
+    public int getCardsPerCoin(int coins) {
+        switch (coins) {
+        case 1:
+            return 4;
+            break;
+        case 2:
+            return 6;
+            break;
+        case 3:
+            return 8;
+            break;
+        case 4:
+            return 10;
+            break;
+        
+        }
+
+    }
+
+};
+
+class Chili : public virtual card {
+    const maxCard = 18;
+    name = "Chili";
+    private string[20] = (name);
+    public int getCardsPerCoin(int coins) {
+        switch (coins) {
+        case 1:
+            return 3;
+            break;
+        case 2:
+            return 6;
+            break;
+        case 3:
+            return 8;
+            break;
+        case 4:
+            return 9;
+                break;
+
+        }
+
+    }
+
+};
+
+class Stink : public virtual card {
+    const maxCard = 16;
+    name = "Stink";
+    private string[20] = (name);
+    public int getCardsPerCoin(int coins) {
+        switch (coins) {
+        case 1:
+            return 3;
+            break;
+        case 2:
+            return 5;
+            break;
+        case 3:
+            return 7;
+            break;
+        case 4:
+            return 8;
+            break;
+
+        }
+
+    }
+};
+
+class green : public virtual card {
+    const maxCard = 14;
+    name = "Green";
+    private string[20] = (name);
+    public int getCardsPerCoin(int coins) {
+        switch (coins) {
+        case 1:
+            return 3;
+            break;
+        case 2:
+            return 5;
+            break;
+        case 3:
+            return 6;
+            break;
+        case 4:
+            return 7;
+            break;
+
+        }
+
+    }
+
+};
+
+class soy : public virtual card {
+    const maxCard = 12;
+    name = "Soy";
+    private string[20] = (name);
+    public int getCardsPerCoin(int coins) {
+        switch (coins) {
+        case 1:
+            return 2;
+            break;
+        case 2:
+            return 4;
+            break;
+        case 3:
+            return 6;
+            break;
+        case 4:
+            return 7;
+            break;
+
+        }
+
+    }
+
+};
+
+class Black : public virtual card {
+    const maxCard = 10;
+    name = "Black";
+    private string[20] = (name);
+    public int getCardsPerCoin(int coins) {
+        switch (coins) {
+        case 1:
+            return 2;
+            break;
+        case 2:
+            return 4;
+            break;
+        case 3:
+            return 5;
+            break;
+        case 4:
+            return 6;
+            break;
+
+        }
+
+    }
+
+};
+
+class red : public virtual card {
+    const maxCard = 8;
+    name = "Red";
+    private string[20] = (name);
+    public int getCardsPerCoin(int coins) {
+        switch (coins) {
+        case 1:
+            return 2;
+            break;
+        case 2:
+            return 3;
+            break;
+        case 3:
+            return 4;
+            break;
+        case 4:
+            return 5;
+            break;
+
+        }
+
+    }
+};
+
+class garde : public virtual card {
+    const maxCard = 6;
+    name = "Garde";
+    private string[20] = (name);
+    public int getCardsPerCoin(int coins) {
+        switch (coins) {
+        case 1:
+            return 0;
+            break;
+        case 2:
+            return 2;
+            break;
+        case 3:
+            return 3;
+            break;
+        case 4:
+            return 0;
+            break;
+
+        }
+        default:
+
+
+    }
+
+};
+
+
+
