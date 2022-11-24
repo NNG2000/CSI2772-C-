2c4a3be9ff163f5791da90960ccd0ed3fc23945b
NNG2000 <55443423+NNG2000@users.noreply.github.com>
2022-11-23 19:34:44 -0500

    I've modified card

--- Projet_Final/Card.h
+++ Projet_Final/Card.h
@@ -1,4 +1,6 @@
-﻿#include <iostream>
+﻿
+#include <iostream>
+#include <iostream>
 using namespace std;
 #include <cassert> //for assert()
 #include <string.h>
@@ -6,246 +8,308 @@ using namespace std;
 enum color { club, diamond, heart, spade };
 
 class Card
-{   
-    public virtual string getName();
-    public virtual int getCardsPerCoin(int coins);
-    public virtual void print(ostream& out);
-    private string name;
-    int point;
-
-
-};
-
-class Blue : public virtual card {
-    const maxCard = 20;
-    name = "Blue";
-    private string[20] = {name};
-    public int getCardsPerCoin(int coins) {
-        switch (coins) {
-        case 1:
-            return 4;
-            break;
-        case 2:
-            return 6;
-            break;
-        case 3:
-            return 8;
-            break;
-        case 4:
-            return 10;
-            break;
-        default:
-            return 0;
-        }
-
-    }
-    public string getName() {
-        return name;
-    }
+{
+public: virtual string getName();
+public: virtual int getCardsPerCoin(int coins);
+public: virtual void print(ostream& out);
+public: friend ostream& operator << (ostream&, Card&);
+      //private : string name;
 
 };
 
-class Chili : public virtual card {
-    const maxCard = 18;
-    name = "Chili";
-    private string[20] = { name };
-    public int getCardsPerCoin(int coins) {
-        switch (coins) {
-        case 1:
-            return 3;
-            break;
-        case 2:
-            return 6;
-            break;
-        case 3:
-            return 8;
-            break;
-        case 4:
-            return 9;
-            break;
-        default:
-            return 0;
+ostream& operator << (ostream& affiche, Card& myCard) {
+    myCard.print(affiche);
+    return affiche;
+};
 
-        }
+class Blue : virtual public Card {
+    const int  maxCard = 20;
+    string name = "Blue";
+private: string cardList[20] = { name };
+public: int getCardsPerCoin(int coins) {
+    int numberCards = 0;
+    switch (coins) {
+    case 1:
+        numberCards = 4;
+        break;
+    case 2:
+        numberCards = 6;
+        break;
+    case 3:
+        numberCards = 8;
+        break;
+    case 4:
+        numberCards = 10;
+        break;
+    default:
+        return 0;
+    }
 
-    }
-    public string getName() {
-        return name;
-    }
+    return numberCards;
+
+}
+public: string getName() {
+    return name;
+}
+public: void print(ostream& out) {
+    char id = 'B';
+    out.put(id);
+}
 
 };
 
-class Stink : public virtual card {
-    const maxCard = 16;
-    name = "Stink";
-    private string[20] = { name };
-    public int getCardsPerCoin(int coins) {
-        switch (coins) {
-        case 1:
-            return 3;
-            break;
-        case 2:
-            return 5;
-            break;
-        case 3:
-            return 7;
-            break;
-        case 4:
-            return 8;
-            break;
-        default:
-            return 0;
-
-        }
-
-    }
-    public string getName() {
-        return name;
-    }
-};
-
-class green : public virtual card {
-    const maxCard = 14;
-    name = "Green";
-    private string[20] = { name };
-    public int getCardsPerCoin(int coins) {
-        switch (coins) {
-        case 1:
-            return 3;
-            break;
-        case 2:
-            return 5;
-            break;
-        case 3:
-            return 6;
-            break;
-        case 4:
-            return 7;
-            break;
-        default:
-            return 0;
-
-        }
+class Chili : virtual public Card {
+    const int maxCard = 18;
+    string name = "Chili";
+private: string cardList[20] = { name };
+public: int getCardsPerCoin(int coins) {
+    int numberCards = 0;
+    switch (coins) {
+    case 1:
+        numberCards = 3;
+        break;
+    case 2:
+        numberCards = 6;
+        break;
+    case 3:
+        numberCards = 8;
+        break;
+    case 4:
+        numberCards = 9;
+        break;
+    default:
+        return 0;
 
     }
-    public string getName() {
-        return name;
-    }
+
+    return numberCards;
+
+}
+public: string getName() {
+    return name;
+}
+public: void print(ostream& out) {
+    char id = 'C';
+    out.put(id);
+}
 
 };
 
-class soy : public virtual card {
-    const maxCard = 12;
-    name = "Soy";
-    private string[20] = { name };
-    public int getCardsPerCoin(int coins) {
-        switch (coins) {
-        case 1:
-            return 2;
-            break;
-        case 2:
-            return 4;
-            break;
-        case 3:
-            return 6;
-            break;
-        case 4:
-            return 7;
-            break;
-        default:
-            return 0;
+class Stink : virtual public Card {
+    const int maxCard = 16;
+    string name = "Stink";
+private: string cardList[20] = { name };
+public: int getCardsPerCoin(int coins) {
+    int numberCards = 0;
+    switch (coins) {
+    case 1:
+        numberCards = 3;
+        break;
+    case 2:
+        numberCards = 5;
+        break;
+    case 3:
+        numberCards = 7;
+        break;
+    case 4:
+        numberCards = 8;
+        break;
+    default:
+        return 0;
+    }
 
-        }
+    return numberCards;
+
+}
+public: string getName() {
+    return name;
+}
+public: void print(ostream& out) {
+    char id = 'S';
+    out.put(id);
+}
+};
+
+class green : virtual public Card {
+    const int maxCard = 14;
+    string name = "Green";
+private: string cardList[20] = { name };
+public: int getCardsPerCoin(int coins) {
+    int numberCards = 0;
+    switch (coins) {
+    case 1:
+        numberCards = 3;
+        break;
+    case 2:
+        numberCards = 5;
+        break;
+    case 3:
+        numberCards = 6;
+        break;
+    case 4:
+        numberCards = 7;
+        break;
+    default:
+        return 0;
 
     }
-    public string getName() {
-        return name;
-    }
+
+    return numberCards;
+
+}
+
+public: string getName() {
+    return name;
+}
+
+public: void print(ostream& out) {
+    char id = 'G';
+    out.put(id);
+}
 
 };
 
-class Black : public virtual card {
-    const maxCard = 10;
-    name = "Black";
-    private string[20] = { name };
-    public int getCardsPerCoin(int coins) {
-        switch (coins) {
-        case 1:
-            return 2;
-            break;
-        case 2:
-            return 4;
-            break;
-        case 3:
-            return 5;
-            break;
-        case 4:
-            return 6;
-            break;
-        default:
-            return 0;
-
-        }
-
+class soy : virtual public Card {
+    const int maxCard = 12;
+    string name = "Soy";
+private: string cardList[20] = { name };
+public: int getCardsPerCoin(int coins) {
+    int numberCards = 0;
+    switch (coins) {
+    case 1:
+        numberCards = 2;
+        break;
+    case 2:
+        numberCards = 4;
+        break;
+    case 3:
+        numberCards = 6;
+        break;
+    case 4:
+        numberCards = 7;
+        break;
+    default:
+        return 0;
     }
-    public string getName() {
-        return name;
-    }
+
+    return numberCards;
+}
+
+public: string getName() {
+    return name;
+}
+
+public: void print(ostream& out) {
+    char id = 'S';
+    out.put(id);
+}
 
 };
 
-class red : public virtual card {
-    const maxCard = 8;
-    name = "Red";
-    private string[20] = { name };
-    public int getCardsPerCoin(int coins) {
-        switch (coins) {
-        case 1:
-            return 2;
-            break;
-        case 2:
-            return 3;
-            break;
-        case 3:
-            return 4;
-            break;
-        case 4:
-            return 5;
-            break;
-
-        }
-        default:
-            return 0;
-
-    }
-    public string getName() {
-        return name;
-    }
-};
-
-class garde : public virtual card {
-    const maxCard = 6;
-    name = "Garde";
-    private string[20] = { name };
-    public int getCardsPerCoin(int coins) {
-        switch (coins) {
-        case 2:
-            return 2;
-            break;
-        case 3:
-            return 3;
-            break;
-
-        }
-        default:
-            return 0;
+class Black : virtual public Card {
+    const int maxCard = 10;
+    string name = "Black";
+private: string cardList[20] = { name };
+public: int getCardsPerCoin(int coins) {
+    int numberCards = 0;
+    switch (coins) {
+    case 1:
+        numberCards = 2;
+        break;
+    case 2:
+        numberCards = 4;
+        break;
+    case 3:
+        numberCards = 5;
+        break;
+    case 4:
+        numberCards = 6;
+        break;
+    default:
+        return 0;
 
     }
-    public string getName() {
-        return name;
-    }
+
+    return numberCards;
+
+}
+
+public: string getName() {
+    return name;
+}
+
+public: void print(ostream& out) {
+    char id = 'B';
+    out.put(id);
+}
 
 };
 
+class red : virtual public Card {
+    const int maxCard = 8;
+    string name = "Red";
+private: string cardList[20] = { name };
+public: int getCardsPerCoin(int coins) {
+    int numberCards = 0;
+    switch (coins) {
+    case 1:
+        numberCards = 2;
+        break;
+    case 2:
+        numberCards = 3;
+        break;
+    case 3:
+        numberCards = 4;
+        break;
+    case 4:
+        numberCards = 5;
+        break;
 
+    default:
+        return 0;
+    }
 
+    return numberCards;
+}
+
+public: string getName() {
+    return name;
+}
+
+public: void print(ostream& out) {
+    char id = 'R';
+    out.put(id);
+}
+};
+
+class garde : virtual public Card {
+    const int maxCard = 6;
+    string name = "Garde";
+private: string cardList[20] = { name };
+public: int getCardsPerCoin(int coins) {
+    int numberCards = 0;
+    switch (coins) {
+    case 2:
+        numberCards = 2;
+        break;
+    case 3:
+        numberCards = 3;
+        break;
+    default:
+        return 0;
+    }
+
+    return numberCards;
+
+}
+
+public: string getName() {
+    return name;
+}
+
+public: void print(ostream& out) {
+    char id = 'G';
+    out.put(id);
+}
+
+};
